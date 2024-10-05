605498f032a6391169e26598d5694e2db98d6f31
Julio M Cruz <julio.cruz@eb-ms.net>
2024-10-05 19:44:44 -0400

    Update Footer.tsx

--- AmigoPago-App/components/Footer.tsx
+++ AmigoPago-App/components/Footer.tsx
@@ -2,11 +2,11 @@ export default function FooterSection() {
     return (
       <footer className="bg-gradient-to-b from-[#f5f6f7] via-[#f5f6f7] to-[#c1c6d0] py-12 w-full">
         <div className="max-w-6xl mx-auto px-4 flex justify-between items-center">
-          <div className="text-xl text-gray-700">© 2024 AmigoPago. All rights reserved</div>
+          <div className="text-sm lg:text-xl text-gray-700">© 2024 AmigoPago. All rights reserved</div>
           <div className="text-right">
-            <p className="text-xl font-semibold text-gray-700">Contact</p>
-            <p className="text-xl text-gray-700">+1-123-456-7890.</p>
-            <p className="text-xl text-gray-700">support@AmigoPago.xyz</p>
+            <p className="text-sm lg:text-xl font-semibold text-gray-700">Contact</p>
+            <p className="text-sm lg:text-xl text-gray-700">+1-123-456-7890.</p>
+            <p className="text-sm lg:text-xl text-gray-700">support@AmigoPago.xyz</p>
           </div>
         </div>
       </footer>
