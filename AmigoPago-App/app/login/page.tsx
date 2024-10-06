29e9ba00456af726f9fde7360007f8ce29a65b10
Julio M Cruz <julio.cruz@eb-ms.net>
2024-10-05 23:48:03 -0400

    Login Component

---
+++ AmigoPago-App/app/login/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import HeaderComponent from "../../components/Header";
+import FooterSection from "../../components/Footer";
+import LoginComponent from "../../components/Login";
+
+export default function Home() {
+  return (
+    <div className="px-10 py-10 h-screen">
+      <div className="space-y-20">
+        <div className="flex flex-col items-center w-full space-y-4">
+          <HeaderComponent />
+          <LoginComponent /> 
+          <FooterSection />
+        </div>
+      </div>
+    </div>    
+  );
+}
