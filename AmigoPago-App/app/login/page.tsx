aa0d2b51a97b470ccf9130a32820169f2f1d48f2
Dev Bambino <devbambinoacc@gmail.com>
2024-10-07 03:03:05 +0000

    UI improvements 3

--- AmigoPago-App/app/login/page.tsx
+++ AmigoPago-App/app/login/page.tsx
@@ -8,7 +8,7 @@ export default function Login() {
   return (
     <div className="h-screen">
       <div className="space-y-20">
-        <div className="flex flex-col items-center w-full space-y-4">
+        <div className="flex flex-col items-center w-full space-y-4 bg-blue-600">
           <HeaderComponent />
           <LoginComponent /> 
           <FooterSection />
--- AmigoPago-App/app/page.tsx
+++ AmigoPago-App/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="">
     <div className="absolute -z-50 top-0 left-0 w-1/2 h-1/2 bg-orange-300/70 blur-[256px] opacity-45 rounded-full -z-1" />
       <div className="space-y-20">
-        <div className="flex flex-col items-center w-full bg-blue-600">
+        <div className="flex flex-col items-center w-full">
           <HeaderComponent />
           <LandingPage />
           <FooterSection />
