507623ad33c12364665cfdae1bd105b5c0efcdac
Dev Bambino <devbambinoacc@gmail.com>
2024-10-07 02:59:43 +0000

    UI improvements 2

--- AmigoPago-App/app/login/page.tsx
+++ AmigoPago-App/app/login/page.tsx
@@ -6,7 +6,7 @@ import LoginComponent from "../../components/Login";
 
 export default function Login() {
   return (
-    <div className="px-10 py-10 h-screen">
+    <div className="h-screen">
       <div className="space-y-20">
         <div className="flex flex-col items-center w-full space-y-4">
           <HeaderComponent />
--- AmigoPago-App/app/signup/page.tsx
+++ AmigoPago-App/app/signup/page.tsx
@@ -6,7 +6,7 @@ import CreateAccountComponent from "../../components/CreateAccount";
 
 export default function SignUp() {
   return (
-    <div className="px-10 py-10">
+    <div className="">
     <div className="absolute -z-50 top-0 left-0 w-1/2 h-1/2 bg-orange-300/70 blur-[256px] opacity-45 rounded-full -z-1" />
       <div className="space-y-20">
         <div className="flex flex-col items-center w-full space-y-4">
